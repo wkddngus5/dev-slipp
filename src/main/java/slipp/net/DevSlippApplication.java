636bea11df7d2b510cc1634bc6426c0e73a952f1
brain <wkddngus5@naver.com>
2017-02-24 17:56:36 +0900

    initial commit

---
+++ src/main/java/slipp/net/DevSlippApplication.java
@@ -0,0 +1,12 @@
+package slipp.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSlippApplication.class, args);
+	}
+}
