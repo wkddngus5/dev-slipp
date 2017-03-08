8514660904bfa446e9f89f0e4555369445770441
brain <wkddngus5@naver.com>
2017-03-08 16:38:45 +0900

    질문 목록 보여주

--- src/main/java/slipp/net/web/HomeController.java
+++ src/main/java/slipp/net/web/HomeController.java
@@ -1,14 +1,20 @@
 package slipp.net.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import slipp.net.domain.QuestionRepository;
+
 @Controller
 public class HomeController {
-
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("questions", questionRepository.findAll());
 		return "/index";
 	}
 }
